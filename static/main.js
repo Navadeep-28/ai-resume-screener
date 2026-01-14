dad23808413e11b3ebd684419216913e230452e4
Navadeep-28 <navadeep285@gmail.com>
2026-01-14 17:49:14 +0530

    Add mode switch initialization on DOMContentLoaded

--- static/main.js
+++ static/main.js
@@ -1,8 +1,9 @@
-// static/main.js
 import { initBatchScreening } from "./batch.js";
 import { initResumeComparison } from "./compare.js";
+import { initModeSwitch } from "./mode-switch.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initBatchScreening();
     initResumeComparison();
+    initModeSwitch();
 });
