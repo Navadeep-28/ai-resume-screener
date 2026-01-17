1436b60119d411ccb33418206d7b85ad040cf3fc
Navadeep-28 <navadeep285@gmail.com>
2026-01-17 19:12:49 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,9 +1,12 @@
 import { initModeSwitch } from "./mode-switch.js";
 import { initExportBatchPDF } from "./export-batch-pdf.js";
 import { initExportComparePDF } from "./export-compare-pdf.js";
+import { initBatchScreening, initBatchCompare } from "./batch.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initModeSwitch();
     initExportBatchPDF();
     initExportComparePDF();
+    initBatchScreening();
+    initBatchCompare();
 });
