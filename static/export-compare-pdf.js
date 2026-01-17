dee6e979aa5d548d0aec16d3447a226241be114e
Navadeep-28 <navadeep285@gmail.com>
2026-01-17 19:37:10 +0530

    Update export-compare-pdf.js

--- static/export-compare-pdf.js
+++ static/export-compare-pdf.js
@@ -1,12 +1,11 @@
 // static/export-compare-pdf.js
-// ============== EXPORT COMPARE RESULTS AS PDF (ES MODULE) ==============
+// ================= EXPORT COMPARE RESULTS PDF (ES MODULE) =================
 
-export function initComparePDFExport() {
+export function initExportComparePDF() {
     const btn = document.getElementById("export-compare-pdf");
     if (!btn) return;
 
     btn.addEventListener("click", () => {
-        // Server generates and returns the PDF
-        window.location.href = "/export-compare-pdf";
+        window.print();
     });
 }
